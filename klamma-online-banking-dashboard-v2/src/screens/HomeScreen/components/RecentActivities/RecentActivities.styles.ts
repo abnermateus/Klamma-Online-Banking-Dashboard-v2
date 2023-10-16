51c5780c37bf24e4337acd509a1c29e0d8b19601
Abner Mateus de Oliveira Santos <mateusabner0@gmail.com>
2023-10-16 19:09:56 -0300

    Update archives.ts

--- klamma-online-banking-dashboard-v2/src/screens/HomeScreen/components/RecentActivities/RecentActivities.styles.ts
+++ klamma-online-banking-dashboard-v2/src/screens/HomeScreen/components/RecentActivities/RecentActivities.styles.ts
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+
+export const RecentActivitiesContainer = styled.div`
+  display: grid;
+  grid-area: recent-activities;
+  gap: 24px;
+  padding: 32px;
+`;
+
+export const RecentActivitiesHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+`;
+
+
